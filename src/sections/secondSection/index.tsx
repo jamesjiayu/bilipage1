d8376b8b8129774311cfd28ad4dddfa7f3683157
James Wang <jamesjiayu@gmail.com>
2023-02-24 08:33:37 -0600

    feat: isFixed on the bottom

--- src/sections/secondSection/index.tsx
+++ src/sections/secondSection/index.tsx
@@ -3,24 +3,30 @@ import CartoonImage from '../../assets/cartoon.jpg'
 import FoodImage from '../../assets/food.jpg'
 import MovieImage from '../../assets/movie.png'
 import LifeImage from '../../assets/life.jpg'
+import ImageLogo from '../../assets/logo.png'
 import style from './style.module.css'
 import classNames from 'classnames'
+const TAB_HEIGHT = 56
 const tabs = [
   {
     key: 'cartoon',
     title: 'Cartoon',
+    image: CartoonImage,
   },
   {
     key: 'food',
     title: 'Food',
+    image: FoodImage,
   },
   {
     key: 'movie',
     title: 'Movie',
+    image: MovieImage,
   },
   {
     key: 'life',
     title: 'Life',
+    image: LifeImage,
   },
 ]
 const SecondSection: FC = () => {
@@ -43,6 +49,14 @@ const SecondSection: FC = () => {
         setIsFixed(false)
       }
       //setIsFixed(top<=0)
+      const sectionNodes = secondSectionRef.current.querySelectorAll('section')
+      Array.from(sectionNodes).forEach((sectionEl) => {
+        const { top } = sectionEl.getBoundingClientRect()
+        const key = sectionEl.getAttribute('data-id') || ''
+        if (top <= TAB_HEIGHT) {
+          setActiveTab(key)
+        }
+      })
     }
   }
   useEffect(() => {
@@ -70,9 +84,17 @@ const SecondSection: FC = () => {
         {tabs.map((tab) => (
           <section data-id={tab.key}>
             <h2>{tab.title}</h2>
-            <img src={CartoonImage} alt={tab.key} />
+            <img src={tab.image} alt={tab.key} />
           </section>
         ))}
+      </div>
+
+      {/* 吸底按钮 */}
+      {/* <div 
+        className={classNames(style.btnWrapper, { [style.visible]: isFixed })}> */}
+      <div className={style.btnWrapper}>
+        <img src={ImageLogo} alt="Logo" />
+        <button>Open in APP</button>
       </div>
     </div>
   )
